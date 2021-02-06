7911cb4b8a2840d6e22048ff9573159da2868dae
Alina_Fitisova <alinafitisova@gmail.com>
2021-02-06 12:37:58 +0200

    last changes

--- #src/js/main.js
+++ #src/js/main.js
@@ -9,7 +9,7 @@ function testWebP(callback) {
     };
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
-    
+
 testWebP(function (support) {
     if (support == true) {
         document.querySelector('body').classList.add('webp');
@@ -26,14 +26,16 @@ const docWidth = document.documentElement.offsetWidth;
 )
 
 // jQuery toggleurger function
+
 ;$(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
     // Modal window DONATE
+
     $(document).ready(function() {
-        $('a.donateLink, a.help__btn').click( function(event){
+        $('.donateLink, a.finance-help__btn').click( function(event){
             event.preventDefault();
             $('#Overlay').fadeIn(297,	function(){
             $('#donatModal') 
@@ -41,14 +43,15 @@ const docWidth = document.documentElement.offsetWidth;
             .animate({opacity: 1}, 198);
             });
         });
-        $('#donatModal__close, #Overlay').click( function(){
+        $('#donatModal__close, #Overlay, .donatModal__btn').click( function(){
             $('#donatModal').animate({opacity: 0}, 198, function(){
                 $(this).css('display', 'none');
                 $('#Overlay').fadeOut(297);
             });
         });
     });
-// jQuery sliders function
+//  sliders function
+
     $('.header-slider').slick({
         dots: true,
         arrows : false,
@@ -71,12 +74,14 @@ const docWidth = document.documentElement.offsetWidth;
         speed: 1000,
         cssEase: 'linear'
     });
-    // jQuery readMore function
-    
+
+    // readMore function
+
     $('.readMore').click(function(event) {
         $('.adopt__text, .back__description').toggleClass('short');
     });
      // To up window button
+    
     $(window).on('scroll', function () {
         if ($(this).scrollTop() > 500) {
         $('.up-button').fadeIn();
@@ -84,6 +89,20 @@ const docWidth = document.documentElement.offsetWidth;
         $('.up-button').fadeOut();
         }
     });
-    
 });
 
+//dynamic changing numbers of the year
+
+const yearBirthFund = new Date().getFullYear() - 2011;
+document.getElementById("yearsFund").innerHTML = yearBirthFund;
+
+// const currentYear = new Date().getFullYear();
+// document.querySelectorAll(".currentYear").innerHTML = currentYear;
+
+
+// set year to .currentYear elements
+const currentYearElements = Array.from(document.querySelectorAll('.currentYear'));
+const now = new Date().getFullYear();
+currentYearElements.forEach(elem => {
+    elem.innerHTML = now;
+});
